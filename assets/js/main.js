7487d14ef0588454e578aa82902e9ac6bc454c7c
EnzoPrina <enzoprina9@gmail.com>
2023-07-15 14:58:47 -0300

    correo configurado

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,3 +197,18 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*  FORMULARIO */
+/*  {} ()  [ ] */
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trucazo')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:me@leonidasesteban.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+}
