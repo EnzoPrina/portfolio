60408fa86d07928e34d5d56796ee2c5227a313b7
EnzoPrina <enzoprina9@gmail.com>
2023-07-16 10:51:57 -0300

    email corregido

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,6 +209,6 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event) {
   event.preventDefault()
   const form = new FormData(this)
-  $buttonMailto.setAttribute('href', `mailto::enzoprina9@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.setAttribute('href', `mailto::enzoprina9@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
   $buttonMailto.click()
 }
